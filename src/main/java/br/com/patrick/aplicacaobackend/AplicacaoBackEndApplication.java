178e1548020cb752d2952cadf842633362c761e5
Patrickaalves <patrickalvessc@gmail.com>
2024-06-18 12:59:50 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/patrick/aplicacaobackend/AplicacaoBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrick.aplicacaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/patrick/aplicacaobackend/AplicacaoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patrick.aplicacaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
