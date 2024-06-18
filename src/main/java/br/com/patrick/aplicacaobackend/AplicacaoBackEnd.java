5aa6bf877988cff69964cf77ba008a54633c53cc
Patrickaalves <patrickalvessc@gmail.com>
2024-06-18 13:03:50 -0300

    Renomeando classes e extensão de arquivos

--- src/main/java/br/com/patrick/aplicacaobackend/AplicacaoBackEnd.java
+++ src/main/java/br/com/patrick/aplicacaobackend/AplicacaoBackEnd.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AplicacaoBackEndApplication {
+public class AplicacaoBackEnd {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AplicacaoBackEndApplication.class, args);
+		SpringApplication.run(AplicacaoBackEnd.class, args);
 	}
 
 }
