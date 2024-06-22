deb2be944ac44b556cbf34ecd7955c74d5dd070e
Patrickaalves <patrickalvessc@gmail.com>
2024-06-22 08:59:12 -0300

    end point para listar todas as persosn

--- src/main/java/br/com/patrick/aplicacaobackend/controller/PersonController.java
+++ src/main/java/br/com/patrick/aplicacaobackend/controller/PersonController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -19,5 +21,10 @@ public class PersonController {
     public Person findByIdPerson(@PathVariable long id) {
         Person buscarPerson = personService.findById(id);
         return buscarPerson;
+    }
+
+    @GetMapping
+    public List<Person> findAllPersons() {
+        return personService.findAll();
     }
 }
--- src/main/java/br/com/patrick/aplicacaobackend/services/PersonService.java
+++ src/main/java/br/com/patrick/aplicacaobackend/services/PersonService.java
@@ -3,6 +3,8 @@ package br.com.patrick.aplicacaobackend.services;
 import br.com.patrick.aplicacaobackend.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -20,6 +22,35 @@ public class PersonService {
 
         person.setId(counter.incrementAndGet());
         person.setFirstName("Patrick");
+        person.setLastName("Alves");
+        person.setEndereco("Joinville - Santa catarina - Brasil");
+        person.setGender("Masculino");
+
+        return person;
+    }
+
+    public List<Person> findAll() {
+
+        logger.info("Buscando todas as persons");
+
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    public Person createPerson(Person person) {
+        return null;
+    }
+
+    public Person mockPerson(int id) {
+        Person person = new Person();
+
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Patrick " + id);
         person.setLastName("Alves");
         person.setEndereco("Joinville - Santa catarina - Brasil");
         person.setGender("Masculino");
